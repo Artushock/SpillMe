0dfab74fff2b8baba9920592328a9d5adc35f3cc
Artushock <nyuar@ukr.net>
2023-08-22 21:13:24 +0200

    Basic main screen implementation

--- app/src/main/java/com/artushock/apps/spillme/navigation/AppNavigation.kt
+++ app/src/main/java/com/artushock/apps/spillme/navigation/AppNavigation.kt
@@ -1,19 +1,29 @@
 package com.artushock.apps.spillme.navigation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -25,11 +35,11 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.artushock.apps.spillme.Plant
+import com.artushock.apps.spillme.R
 import com.artushock.apps.spillme.dev.PlantsProvider
 import com.artushock.apps.spillme.ui.theme.MainBeige
 import com.artushock.apps.spillme.ui.theme.MainBrown
 import com.artushock.apps.spillme.ui.theme.MainGreen
-import com.artushock.apps.spillme.ui.theme.PurpleGrey40
 
 @Composable
 fun AppNavigation() {
@@ -44,16 +54,28 @@ fun AppNavigation() {
 fun MainListScreen(navController: NavHostController) {
     val plants = PlantsProvider().getPlants()
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(
-            items = plants,
-            itemContent = { PlantItem(plant = it) }
-        )
+    Box {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(
+                items = plants,
+                itemContent = { PlantItem(plant = it) }
+            )
+        }
+        FloatingActionButton(
+            onClick = { },
+            shape = CircleShape,
+            containerColor = MainGreen,
+            modifier = Modifier
+                .padding(48.dp)
+                .align(Alignment.BottomEnd),
+        ) {
+            Image(painter = painterResource(id = R.drawable.ic_plus_24), contentDescription = null)
+        }
     }
 }
 
@@ -61,43 +83,84 @@ fun MainListScreen(navController: NavHostController) {
 fun PlantItem(plant: Plant) {
     Column(
         Modifier
-            .clip(shape = RoundedCornerShape(4.dp))
+            .shadow(3.dp, shape = RoundedCornerShape(8.dp))
+            .clip(shape = RoundedCornerShape(6.dp))
             .fillMaxSize()
             .background(MainBeige)
 
     ) {
         Text(
             text = plant.name,
-            modifier = Modifier.padding(8.dp).fillMaxSize(),
-            style = TextStyle(color = MainBrown, fontSize = 18.sp, fontWeight = FontWeight.Bold),
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize(),
+            style = TextStyle(color = MainBrown, fontSize = 16.sp, fontWeight = FontWeight.Bold),
             textAlign = TextAlign.Center
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .height(2.dp)
+                .background(MainBrown)
         )
         AsyncImage(
             model = plant.photo,
             contentDescription = "Plant photo",
-            modifier = Modifier.padding(top = 8.dp)
         )
-    }
-}
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .height(2.dp)
+                .background(MainBrown)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MainBeige)
+                .padding(8.dp, 8.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
 
-@Composable
-fun Foo() {
-    Column(
-        Modifier
-            .padding(8.dp)
-            .fillMaxSize()
-            .background(MainGreen)
-            .clip(shape = RoundedCornerShape(16.dp))
-    ) {
-        Text(
-            text = "plant.name",
-            modifier = Modifier.padding(8.dp),
-            style = TextStyle(color = PurpleGrey40, fontSize = 18.sp, fontWeight = FontWeight.Bold)
-        )
-        AsyncImage(
-            model = "plant.photo",
-            contentDescription = "Plant photo",
-            modifier = Modifier.padding(top = 8.dp)
-        )
+
+            ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_watering),
+                    contentDescription = null
+                )
+                Text(
+                    text = "12 days",
+                    modifier = Modifier
+                        .padding(8.dp),
+                    style = TextStyle(color = MainBrown, fontSize = 11.sp),
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_feeding),
+                    contentDescription = null
+                )
+                Text(
+                    text = "12 days",
+                    modifier = Modifier
+                        .padding(8.dp),
+                    style = TextStyle(color = MainBrown, fontSize = 11.sp),
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_spray),
+                    contentDescription = null
+                )
+                Text(
+                    text = "12 days",
+                    modifier = Modifier
+                        .padding(8.dp),
+                    style = TextStyle(color = MainBrown, fontSize = 11.sp),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
